11d327f5b5442ff9dbc0153535bed98c6908f9f5
liuqixin <liuqixin@qq.com>
2022-02-16 12:19:54 +0800

    spring整合kafka实现发送系统通知、显示系统通知

--- src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
+++ src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
@@ -3,7 +3,6 @@ package com.nowcoder.community.aspect;
 import com.nowcoder.community.util.CommonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
